056ca247318d98304cc4110d1324b5f74e221ae2
Rafael <rafesilvadesouza@gmail.com>
2023-12-17 17:17:20 -0300

    Wokring on login

--- client/src/firebase-config.ts
+++ client/src/firebase-config.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAasfg_AXcHYKDb-xYhHZoaS7x5ZmgwFz4',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'weather-app-9209e.firebaseapp.com',
   projectId: 'weather-app-9209e',
   storageBucket: 'weather-app-9209e.appspot.com',
